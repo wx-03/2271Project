41fde180fb6e15f63f796e194002e303368fe7d5
wx-03 <tngwenxi@gmail.com>
2024-10-29 11:43:56 +0800

    Add ending music, fix running mode led

--- main.c
+++ main.c
@@ -24,6 +24,7 @@ volatile uint8_t uartData = 0x77;
 volatile float leftDc = 0.0;
 volatile float rightDc = 0.0;
 volatile static uint8_t isMoving;
+volatile static uint8_t isDone;
 
 uint32_t frequencies_mod[] = {1000};
 
@@ -217,7 +218,14 @@ void green_blinky_main(void *argument)
 	for (;;) {
 		if (isMoving)
 		{
-			// Running Mode
+			for (int i = 0; i < 10; i++) {
+				if (0 <= i && i <= 3) {
+					PTB->PCOR |= MASK(greenPins[i]);
+				} else {
+					PTE->PCOR |= MASK(greenPins[i]);
+				}
+			}
+			// Running Mode			
 			for (int i = 0; i < 10; i++) {
 				if (0 <= i && i <= 3) {
 					PTB->PTOR |= MASK(greenPins[i]);
@@ -252,11 +260,17 @@ void buzz_main(void *argument)
 
   for (;;)
   {
+		
     change_frequency(melody[i]);
     osDelay(noteDurations[i]);
     // change_frequency(0);
     // osDelay(5);
-    i = (i + 1) % (sizeof(melody) / sizeof(melody[0]));
+		int length = sizeof(melody) / sizeof(melody[0]);
+		if (isDone == 0) {
+			i = (i + 1) % length;
+		} else {
+			i = (i == 0) ? length - 1 : i - 1;
+		}
   }
 }
 
